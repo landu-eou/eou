88ee7b5f57b1f332d52cafa0c945da71738a76a0
landu-eou <landueve78@gmail.com>
2026-03-09 02:47:55 +0100

    Enhance SQL query with explanatory comments
    Added comments to clarify the purpose of the query and the use of UNNEST.

--- sql/eou_corporationKills_bq-gh-esi-bq_ids.sql
+++ sql/eou_corporationKills_bq-gh-esi-bq_ids.sql
@@ -1,3 +1,6 @@
+-- Extrae todos los corporation_id únicos presentes en la tabla fuente.
+-- corporationID es un ARRAY<STRUCT<corporation_id INT64>>, por eso se usa UNNEST.
+
 SELECT DISTINCT
   corp.corporation_id AS corporation_id
 FROM `{{SOURCE_TABLE}}`,
