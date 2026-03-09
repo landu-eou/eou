c7e0f85139ef05a57d292c5f88fe30c5f250d69b
landu-eou <landueve78@gmail.com>
2026-03-09 01:52:45 +0100

    Update eou_corporationKills_bq-gh-esi-bq_ids.sql

--- sql/eou_corporationKills_bq-gh-esi-bq_ids.sql
+++ sql/eou_corporationKills_bq-gh-esi-bq_ids.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT
   corp.corporation_id AS corporation_id
-FROM `eou-ht.eou.gate_kills`,
+FROM `{{SOURCE_TABLE}}`,
 UNNEST(corporationID) AS corp
 WHERE corp.corporation_id IS NOT NULL
 ORDER BY corporation_id ASC
